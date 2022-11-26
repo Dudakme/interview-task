35cf8671a7cb53c6cb98c88d0f6b2f83b12f1655
D <61399248+BotGUyz@users.noreply.github.com>
2022-11-26 07:42:53 +0700

    반응 리포 제작
    findResponse 완성.

--- src/structures/repository/communication.repo.ts
+++ src/structures/repository/communication.repo.ts
@@ -1,8 +1,54 @@
+interface response {
+    reply: string,
+    message: string,
+    likability: number
+}
+
+const responses: response[] = [
+    {
+        message: '안녕',
+        reply: '안녕하세요!',
+        likability: 1
+    },
+    {
+        message: '안녕',
+        reply: '안녕하세요 !author님!',
+        likability: 1
+    },
+    {
+        message: '좋아해',
+        reply: '저도 저도 !author님이 좋아요!',
+        likability: 3
+    },
+    {
+        message: '저리가',
+        reply: '...그런 말 하시면 슬퍼요...',
+        likability: -2
+    }
+]
+
 
 class CommunicationRepo {
     constructor() {
 
     }
-
     
+    private async findResponse(message: string): Promise<response | undefined> {
+
+        // db 일 경우 대비해 await 작성
+        return await responses.find(e => e.message === message)
+    }
+
+    private async deleteResponse(message: string) {
+
+    }
+
+    private async updateResponse(message: string) {
+
+    }
+
+    private async createResponse(input: response) {
+
+    }
+
 }

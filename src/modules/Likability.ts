08575164ed357ce15f4dff41e588eefdd39da190
D <61399248+BotGUyz@users.noreply.github.com>
2022-12-09 19:55:28 +0700

    호감도 업데이트

--- src/modules/Likability.ts
+++ src/modules/Likability.ts
@@ -10,11 +10,12 @@ import {
     ChatInputCommandInteraction,
     Message,
     ApplicationCommandOptionType,
+    User,
   } from "discord.js"
   
   import userService from "../structure/service/user.service"
   import { UserRepository, LikabilityRepository } from ".."
-  import { likabilityEmbed } from "../structure/Embed"
+  import { likabilityEmbed } from "../structure/replyTools"
   import { IUser } from "../structure/models"
   
   class PingPongExtension extends Extension {
@@ -25,10 +26,19 @@ import {
       type: ApplicationCommandType.ChatInput,
       description: "크시와 얼마나 친한지 확인할 수 있어요!",
     })
-    async likability(i: ChatInputCommandInteraction) {
-  
-      const userData: IUser = await this.users.findUser(i.user.id)
-      await i.reply({ embeds: [likabilityEmbed(i.user, userData.likability, userData.getLikeLevel())]})
+    async likability(i: ChatInputCommandInteraction, @option({
+      name: "user",
+      type: ApplicationCommandOptionType.User,
+      description: "호감도를 확인 할 수 있어요!",
+    }) userOptional: string) {
+
+      const userTargetId = userOptional ? userOptional : i.user.id;
+      const userData = await this.users.findUser(userTargetId)
+      const userObject = this.client.users.cache.find(user => user.id === userTargetId) 
+
+      if (!userData) return i.reply('갸우뚱')
+
+      await i.reply({ embeds: [likabilityEmbed(userObject ? userObject.username : '갸우뚱', userData.likability, userData.getLikeLevel())]})
     }
   }
   

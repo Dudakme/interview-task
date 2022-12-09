a88fe15698d4523635dcb5603db8856d9dd7d948
D <61399248+BotGUyz@users.noreply.github.com>
2022-12-09 19:55:05 +0700

    update badges and userRepo

--- src/modules/badges.ts
+++ src/modules/badges.ts
@@ -8,13 +8,17 @@ import {
 import {
   ApplicationCommandType,
   ChatInputCommandInteraction,
-  Message,
+  Events,
+  Interaction,
+  StringSelectMenuBuilder,
+  ActionRowBuilder,
   ApplicationCommandOptionType,
+  APISelectMenuOption
 } from "discord.js"
 
 import userService from "../structure/service/user.service"
 import { UserRepository, LikabilityRepository } from ".."
-import { likabilityEmbed } from "../structure/Embed"
+import { badgeEmbed, BadgeOptionalEmbed, BadgeRow, likabilityEmbed } from "../structure/replyTools"
 import { IUser, IBadge } from "../structure/models"
 
 class PingPongExtension extends Extension {
@@ -23,13 +27,61 @@ class PingPongExtension extends Extension {
   @applicationCommand({
     name: "배지",
     type: ApplicationCommandType.ChatInput,
-    description: "크시와 얼마나 친한지 확인할 수 있어요!",
+    description: "크시와 얼마나 친한지 확인할 수 있어요!", 
   })
-  async badge(i: ChatInputCommandInteraction) {
+  async badge(i: ChatInputCommandInteraction, @option({
+    name: "user",
+    type: ApplicationCommandOptionType.User,
+    description: "호감도를 확인 할 수 있어요!",
+  }) userOptionalId: string) {
+
+    if (!userOptionalId) {
+      userOptionalId = i.user.id
+    }
+    const badgeData: IBadge[] = await this.users.getBadges(userOptionalId)
+
+  
+
+    await i.reply({
+      content: `<@${i.user.id}>`,
+      embeds: [badgeEmbed()],
+      components: [BadgeRow(badgeData)],
+    })
+  }
+
+  @applicationCommand({
+    name: "배지랭킹",
+    type: ApplicationCommandType.ChatInput,
+    description: "크시와 얼마나 친한지 확인할 수 있어요!", 
+  })
+  async badgeRanking(i: ChatInputCommandInteraction) {
+    await i.reply({
+      content: '아주 평범한 크시의 배지 랭킹이에요!', components: [new ActionRowBuilder<StringSelectMenuBuilder>()
+        .addComponents(
+          new StringSelectMenuBuilder()
+            .setPlaceholder("가장 많은 크시 배지를 발견한 사람들")
+            .setCustomId('badgeRanking')
+            .addOptions((await UserRepository.getRankers()).map<APISelectMenuOption>((v, i) => {
+              return {
+                label: v.username,
+                description: `${i + 1}등입니다!`,
+                value: `${i}placement`
+              }
+            }))
+        )
+      ]
+    })
+  }
 
 
-    const badgeData: IBadge[] = await this.users.getBadges(i.user.id)
-    await i.reply(badgeData.toString())
+  @listener({
+    event: Events.InteractionCreate,
+  })
+  async badgeInteraction(i: Interaction) {
+    if (!i.isStringSelectMenu()) return
+
+    await i.deferUpdate()
+    await i.editReply({ embeds: [BadgeOptionalEmbed(i.values[0])] })
   }
 }
 
--- src/structure/repository/user.repository.ts
+++ src/structure/repository/user.repository.ts
@@ -1,5 +1,5 @@
 import { BadgeModel, UserModel } from "../entities"
-import { IBadge, InputUser } from "../models"
+import { IBadge, InputUser, IUser } from "../models"
 
 export default class UserRepo {
   constructor(
@@ -8,23 +8,36 @@ export default class UserRepo {
   ) {}
 
   // 임시적으로 any 사용. 테스트 용, 의존성 주입을 위하여 따로 인수 제작
-  public async getUserbyId(id: string): Promise<any> {
+  public async getUserbyId(id: string): Promise<IUser | null> {
     return await this.user.findOne({ id: id })
   }
 
   public async getUserBadges(id: string): Promise<IBadge[]> {
-    const badges: IBadge[] = await this.badges.find({ id: id })
+    const badges: IBadge[] = await this.badges.find({ owner: id })
 
     return badges
   }
 
   public async createBadge(options: IBadge): Promise<boolean> {
     const badge = new this.badges(options)
+    badge.save()
 
-      badge.save((err) => {
-        if (err) return false
-      })
-      return true
+    const user = await this.user.findOne({ id: options.owner })
+
+    if (!user) return false;
+
+    await this.user.updateOne({ id: options.owner }, { badges: [...user.badges, { owner: options.owner, badgeId: options.badgeId}] })
+
+    return true
+  }
+
+  public async getRankers(): Promise<InputUser[]> {
+    const collection = await this.user.find()
+
+    return collection.sort((a, b) => {
+      return a.badges.length - b.badges.length
+    })
+
   }
 
   // 임시적으로 any 사용. 테스트 용
@@ -41,16 +54,17 @@ export default class UserRepo {
   public async createUser(options: InputUser) {
     const res = new this.user(options)
 
-    res.save((err) => {
+    res.save(async (err) => {
       if (err) return err
 
-      const badge = new this.badges({
-        badgeId: "0",
+      await this.createBadge({
+        badgeId: 0,
         owner: res.id,
       })
 
-      badge.save((err) => {
-        if (err) return err
+      await this.createBadge({
+        badgeId: 1,
+        owner: res.id,
       })
     })
 
